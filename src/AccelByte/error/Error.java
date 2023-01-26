01f6f92ec3fda2e628ce24fbff8afed1ccbd81af
Niral H Lad <43843579+niralhlad@users.noreply.github.com>
2023-01-26 12:11:23 -0500

    Final

---
+++ src/AccelByte/error/Error.java
@@ -0,0 +1,34 @@
+package AccelByte.error;
+
+/**
+ Error class has all the com.com.error.error raised by the application.
+ */
+public class Error {
+    public String validInput(){
+        return "Please Try Again with Valid Input format: sync -s source_folder -d target_folder";
+    }
+    public String invalidNumberofArguments() {
+        return "ERR : INVALID NUMBER OF ARGUMENTS (syncfolders.exe excepts 5 arguments)\n" + validInput();
+    }
+    public String noCommandFound() {
+        return "ERR : NO SUCH COMMAND FOUND => \"%USER_CMD%\"\n" + validInput();
+    }
+    public String invalidArguments(){
+        return "ERR : INVALID ARGUMENTS => \"%USER_CMD%\"\n" + validInput();
+    }
+    public String invalidFolderName(){
+        return "ERR : INVALID FOLDER NAME => \"%FOLDER_NAME%\"";
+    }
+    public String folderAccessError(){
+        return "ERR : NOT ABLE TO OPEN FOLDER => \"%FOLDER_NAME%\" \nPlease check folder name and/or path";
+    }
+    public String syncingError(){
+        return "ERR : ERROR WHILE SYNCING";
+    }
+    public String undetectedError() {
+        return "ERR: UNDETECTED ERROR OCCURRED\n" + validInput();
+    }
+}
+
+
+
---
+++ src/testAccelByte/helper/HelperTest.java
@@ -0,0 +1,73 @@
+package testAccelByte.helper;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+//custom imports
+import AccelByte.helper.Helper;
+import AccelByte.error.Error;
+
+/**
+ HelperTest class has all the test cases to tes the functions of Helper Class
+ */
+public class HelperTest {
+    Helper helper = new Helper();
+    Error error = new Error();
+
+    /**
+     * This method is to test correct input with source first and destination second
+     */
+    @Test
+    public void testValidInput() {
+        assertEquals("source<-d>destination",helper.checkAndParseArgs("sync -s source -d destination".split(" ")));
+    }
+    /**
+     * This method is to test correct input with source second and destination first
+     */
+    @Test
+    public void testValidInput2() {
+        assertEquals("source<-d>destination",helper.checkAndParseArgs("sync -d destination -s source".split(" ")));
+    }
+    /**
+     * This method is to test incorrect number of arguments
+     */
+    @Test
+    public void testInvalidNumberofArgs(){
+        assertEquals(error.invalidNumberofArguments() , helper.checkAndParseArgs("sync -s source -d destination error".split( " ")));
+    }
+    /**
+     * This method is to test incorrect command
+     */
+    @Test
+    public void testInvalidCommand(){
+        assertEquals(error.noCommandFound().replaceFirst("%USER_CMD%","testCommand") , helper.checkAndParseArgs("testCommand -s source -d destination".split( " ")));
+    }
+    /**
+     * This method is to test with no source argument
+     */
+    @Test
+    public void testNoSourceArgument(){
+        assertEquals(error.invalidArguments().replaceFirst("%USER_CMD%","sync -e source -d destination") , helper.checkAndParseArgs("sync -e source -d destination".split( " ")));
+    }
+    /**
+     * This method is to test with no source argument
+     */
+    @Test
+    public void testNoDestinationArgument(){
+        assertEquals(error.invalidArguments().replaceFirst("%USER_CMD%","sync -s source -e destination") , helper.checkAndParseArgs("sync -s source -e destination".split( " ")));
+    }
+    /**
+     * This method is to test folder name containing invalid characters
+     */
+    @Test
+    public void testInvalidFolderName(){
+        assertEquals(error.invalidFolderName().replaceFirst("%FOLDER_NAME%","sou<rce") , helper.checkAndParseArgs("sync -s sou<rce -d destination".split( " ")));
+    }
+    /**
+     * This method is to test folder name containing invalid characters
+     */
+    @Test
+    public void testInvalidFolderName2(){
+        assertEquals(error.invalidFolderName().replaceFirst("%FOLDER_NAME%","{source") , helper.checkAndParseArgs("sync -s {source -d destination".split( " ")));
+    }
+}
